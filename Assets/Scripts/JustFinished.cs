372bab8e51a3c701cfef5fc5a4193ab1b8d9f8b3
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2024-03-05 15:59:40 -0600

    Added scenes to build index, fixed finish line functionality.

--- Assets/Scripts/JustFinished.cs
+++ Assets/Scripts/JustFinished.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class JustFinished : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Finished"))
+        if (other.gameObject.name == "Player")
         {
             Debug.Log("You have just crossed the finish line!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
