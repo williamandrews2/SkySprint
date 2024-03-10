a18631d858c11a825fc440aa9b052852b889d1d9
williamandrews2 <121400607+williamandrews2@users.noreply.github.com>
2024-03-10 17:18:15 -0500

    Created orbital camera for third person view in MouseLookAround.cs.

--- Assets/Scripts/MouseLookAround.cs
+++ Assets/Scripts/MouseLookAround.cs
@@ -8,10 +8,19 @@ public class MouseLookAround : MonoBehaviour
     float rotationY = 0f;
 
     [SerializeField] public float sensitivity = 3f;
+    [SerializeField] float orbitDampening = 10f;
+
+    Vector3 rotation;
     void Update()
     {
-        rotationY += Input.GetAxis("Mouse X") * sensitivity;
-        rotationX += Input.GetAxis("Mouse Y") * -1 * sensitivity;
-        transform.localEulerAngles = new Vector3 (rotationX, rotationY, 0);
+        rotation.x += Input.GetAxis("Mouse X") * sensitivity;
+        rotation.y -= Input.GetAxis("Mouse Y") * sensitivity;
+
+        rotation.y = Mathf.Clamp(rotation.y, -89.9f, 89.9f);
+
+        //transform.localEulerAngles = new Vector3 (rotationX, rotationY, 0);
+
+        Quaternion QT = Quaternion.Euler(rotation.y, rotation.x, 0f);
+        transform.rotation = Quaternion.Lerp(transform.rotation, QT, Time.deltaTime * orbitDampening);
     }
 }
